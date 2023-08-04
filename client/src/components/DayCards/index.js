d18cacb414de1661a05bc3b3e6225c9a0b97c8ae
Juice <hugoolivares01@gmail.com>
2023-08-03 18:30:03 -0700

    Footer fix

--- client/src/components/DayCards/index.js
+++ client/src/components/DayCards/index.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';
 
 
 
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import DayCards from '../Components/DayCards';
+import './homepage.css'
 
 function Profile() {
     return (
