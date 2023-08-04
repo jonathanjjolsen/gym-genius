b7afccc9632e1478c11d20d574a3f4b6040771dc
Laptop (Brent) <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-08-03 20:58:32 -0700

    added missing signup link

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
               <p className='text-light fs-3'>With an intuitive interface, simply browse through a wide selection of exercises from various categories including cardio, upper body, and more. Say goodbye to dull routines and embrace the genius of tailored workouts with Gym Genius!</p>
             </div>
             <div className='text-center'>
-              <Link to={'/'} className='btn btn-light rounded-pill py-3 px-5 fs-3 fw-bold mt-3 mb-5'>Join Now</Link>
+              <Link to={'/signUp'} className='btn btn-light rounded-pill py-3 px-5 fs-3 fw-bold mt-3 mb-5'>Join Now</Link>
             </div>
           </div>
         </div>
