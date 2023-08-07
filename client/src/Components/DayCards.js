e6dd35731bb68a68ca00e12e7ca2a2ca0d4ce004
Juice <hugoolivares01@gmail.com>
2023-08-07 16:32:07 -0700

    styles

--- client/src/Components/DayCards.js
+++ client/src/Components/DayCards.js
@@ -1,30 +1,48 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const DayCards = ({week}) => {
-
-    
+const DayCards = ({ week }) => {
 
 
 
-    return (
-              <div id='DayCard'>
-                <button id='DayCardButton'>Edit</button>
-                {week.map((day, index) => (
-                  <div id='DayCardInner'
-                  key={index}>
-                    <h3>{day.Day}</h3>
-                    <h4>Workout:</h4>
-                    <p>{day.WorkoutName}</p>
-                    <h4>Excercises:</h4>
-                    <p>{day.Exercise.join(', ')}</p>
-                    <h4>Goals:</h4>
-                    <p>{day.Goal.join(', ')}</p>
-                  </div>
-                ))}
-              </div>
-            );
-          };
-          
+
+
+  return (
+    <div id='DayCard'>
+
+      <button id='DayCardButton'>Edit</button>
+
+      <h1> Workouts</h1>
+
+      {week.map((day, index) => (
+        <div id='DayCardInner'
+          key={index}>
+
+          <h3>{day.Day}</h3>
+          <div id="CardBottom">
+            <div id="CardBottomLeft">
+              <h4>Workout:</h4>
+              <p>{day.WorkoutName}</p>
+            </div>
+
+            <div id="CardBottomMiddle">
+              <h4>Excercises:</h4>
+              <p>{day.Exercise.join(', ')}</p>
+            </div>
+
+            <div id="CardBottomRight">
+              <h4>Goals:</h4>
+              <p>{day.Goal.join(', ')}</p>
+            </div>
+          </div>
+
+
+
+        </div>
+      ))}
+    </div>
+  );
+};
+
 
 export default DayCards;
