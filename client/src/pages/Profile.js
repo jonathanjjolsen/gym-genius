453f92e3b548e932318856616d62718aaddd60bc
Juice <hugoolivares01@gmail.com>
2023-08-08 15:32:45 -0700

    first 7 days

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -9,63 +9,12 @@ import renderAvatar from '../Components/Avatar';
 import swal from 'sweetalert';
 
 
-const WeekWorkout = [
-  {
-    WorkoutName: "Chest",
-    Exercise: ["Bench", "Incline Bench", "Decline Bench"],
-    Goal: ["Benching 200lbs", "Incline 150lbs", "Decline 150lbs"]
-  },
-  {
-    WorkoutName: "Cardio",
-    Exercise: ["Running", "Biking", "Swimming", "Rowing", "Jump Rope", "Stairmaster", "Elliptical"],
-    Goal: []
-  },
-  {
-    WorkoutName: "Arms",
-    Exercise: ["Bicep Curls", "Tricep Extensions", "Hammer Curls"],
-    Goal: ["Bicep Curls 200lbs", "Tricep Extensions 150lbs", "Hammer Curls 150lbs", "Bicep Curls 200lbs", "Tricep Extensions 150lbs", "Hammer Curls 150lbs"]
-  },
-  {
-    WorkoutName: "Abs",
-    Exercise: ["Crunches", "Situps", "Planks"],
-    Goal: ["Crunches 200lbs", "Situps 150lbs", "Planks 150lbs"]
-  },
-  {
-    WorkoutName: "",
-    Exercise: [],
-    Goal: []
-  },
-  {
-    WorkoutName: "Legs",
-    Exercise: ["Squats", "Lunges"],
-    Goal: ["Squats 200lbs", "Lunges 150lbs"]
-  },
-  {
-    WorkoutName: "Back",
-    Exercise: ["Deadlifts", "Pullups", "Rows"],
-    Goal: ["Deadlifts 200lbs", "Pullups 150lbs", "Rows 150lbs"]
-  }
-];
-
-const WeekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
-const Week = WeekWorkout.map((Workout, i) => {
-  return {
-    Day: WeekDays[i],
-    WorkoutName: Workout.WorkoutName,
-    Exercise: Workout.Exercise,
-    Goal: Workout.Goal
-  };
-});
-
 function Profile() {
-  const [week, setWeek] = useState(Week);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { loading, data } = useQuery(GET_ME);
   const userData = data?.user || {};
   const [selectedFile, setSelectedFile] = useState(null);
   const [message, setMessage] = useState('');
-  console.log(userData);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -210,7 +159,7 @@ function Profile() {
         </div>
       </div>
       <div id="ProfileBottom">
-        <DayCards week={week} />
+        <DayCards week={userData} />
         <div id="ProfileWorkoutGoals">
           <h2>Workout Goals:</h2>
           <p>{userData.Goals}</p>
