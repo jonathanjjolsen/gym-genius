83effcec398c10e224bd258a750eb629530cd53c
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-08-07 18:36:04 -0700

    Avatar functionality with AWS S3 Bucket

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+// Amazon Imports below 
+const AwsClient = require('./utils/client');
+const { ListBucketsCommand, PutObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
+const multer = require('multer');
+const fs = require('fs');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,6 +18,9 @@ const server = new ApolloServer({
     resolvers,
     context: authMiddleware
 });
+
+//Multer helper for uploading files
+const upload = multer({ dest: 'temp/' });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -22,6 +31,69 @@ if (process.env.NODE_ENV === 'production') {
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// GET route to list buckets (as before)
+app.get('/buckets', async (req, res) => {
+    try {
+        const command = new ListBucketsCommand({});
+        const response = await AwsClient.s3Instance.send(command);
+        res.send(response.Buckets);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+});
+
+//Post Route to upload the Avatar photo
+app.post('/upload', upload.single('file'), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).send('No file uploaded');
+        }
+
+        const fileContent = fs.readFileSync(req.file.path);
+        const params = {
+            Bucket: 'gym-genius',
+            Key: req.file.originalname,
+            Body: fileContent
+        };
+
+        const command = new PutObjectCommand(params);
+        await AwsClient.s3Instance.send(command);
+
+        fs.unlinkSync(req.file.path);
+
+        res.send('File uploaded successfully!');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error uploading the file');
+    }
+});
+
+
+//Delete Route to delete the Avatar photo
+app.delete('/delete/:filename', async (req, res) => {
+    try {
+        const filename = req.params.filename;
+
+        if (!filename) {
+            return res.status(400).send('Filename not provided');
+        }
+
+        const params = {
+            Bucket: 'gym-genius',
+            Key: filename
+        };
+
+        const deleteCommand = new DeleteObjectCommand(params);
+        await AwsClient.s3Instance.send(deleteCommand);
+
+        res.send(`File ${filename} deleted successfully!`);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error deleting the file');
+    }
 });
 
 const startServer = async () => {
