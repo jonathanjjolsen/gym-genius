c5a4929641828e6b0d9bc0191e464db02154cd42
Brent <124935518+Brent-Bew-Canon@users.noreply.github.com>
2023-08-08 11:20:34 -0700

    Workout page populated with redirect

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -4,9 +4,11 @@ import { GET_EXERCISES } from '../utils/queries';
 import { useMutation } from '@apollo/client';
 import { CREATE_WORKOUT } from '../utils/mutations';
 import AuthService from '../utils/auth';
+import { useNavigate } from 'react-router-dom';
 import './styles.css';
 
 const Categories = () => {
+    const navigate = useNavigate();
 
     const [selectedExercises, setSelectedExercises] = useState([]);
 
@@ -53,7 +55,7 @@ const Categories = () => {
             console.log('You must be logged in to create a workout');
             return;
         }
-        
+
         try {
             const response = await createWorkout({
                 variables: {
@@ -61,6 +63,8 @@ const Categories = () => {
                     selectedExercises: selectedExercises.map(exercise => exercise._id)
                 }
             })
+            navigate("/Workout");
+            navigate(0)
             console.log(response);
         } catch (err) {
             console.error(err);
@@ -76,7 +80,7 @@ const Categories = () => {
             {selectedExercises?.length > 0 && (
                 <div className="selected-exercises mb-4 text-white">
                     <h2 className='underline'>Selected Exercises</h2>
-                    <input type="text" placeholder="Workout Name" value={workoutName} onChange={handleWorkOutNameChange}/>
+                    <input type="text" placeholder="Workout Name" value={workoutName} onChange={handleWorkOutNameChange} />
                     {selectedExercises.map(exercise => (
                         <div key={exercise.id} className='m-3'>
                             <h3>{exercise.name}</h3>
@@ -117,8 +121,6 @@ const Categories = () => {
                     </div>
                 </div>
             ))}
-
-
         </div>
     );
 }
--- client/src/pages/Workout.js
+++ client/src/pages/Workout.js
@@ -1,16 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
 
 
 function Workout() {
+
+    useEffect(() => {
+
+    }, [])
+
+    const { loading, data } = useQuery(GET_ME);
+    const userData = data?.user || {};
+    console.log(userData);
+
     return (
         <div className='container'>
             <div className='row mt-5'>
                 <div className='col-12 text-center text-white'>
-                    <h1>Here is the workouts page in progress...</h1>
+                    <h1 className='mb-5'>Your Workouts</h1>
+                    <div>
+                        {userData.workouts?.length > 0 && (
+                            <div>
+                                {userData.workouts.map(workout => (
+                                    <div key={workout._id} class="card text-center mb-5">
+                                        <div className="card-header fs-2 bg-dark py-2" >
+                                            {workout.workoutName}
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title"></h5>
+                                            {workout.exercises.map(exercise => (
+                                                <p class="card-text fs-4" key={exercise._id} >{exercise.name}</p>
+                                            ))}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
